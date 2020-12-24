e8291905c40ed59c9c5aaecdec29bac9a0803781
jackieli123723 <380863274@qq.com>
2020-12-24 15:10:02 +0800

    bizcharts row props 渲染

--- src/pages/chart/chart.tsx
+++ src/pages/chart/chart.tsx
@@ -180,6 +180,80 @@ const BizCharts: React.FC<{}> = (props) => {
     },
   ])
 
+  const [childSource,setChildSource] = useState([
+      {
+          State: "AL",
+          "Under 5 Years": 310504,
+          "5 to 13 Years": 552339,
+          "14 to 17 Years": 259034,
+          "18 to 24 Years": 450818,
+          "25 to 44 Years": 1231572,
+          "45 to 64 Years": 1215966,
+          "65 Years and Over": 641667
+        },
+        {
+          State: "AK",
+          "Under 5 Years": 52083,
+          "5 to 13 Years": 85640,
+          "14 to 17 Years": 42153,
+          "18 to 24 Years": 74257,
+          "25 to 44 Years": 198724,
+          "45 to 64 Years": 183159,
+          "65 Years and Over": 50277
+        },
+        {
+          State: "AZ",
+          "Under 5 Years": 515910,
+          "5 to 13 Years": 828669,
+          "14 to 17 Years": 362642,
+          "18 to 24 Years": 601943,
+          "25 to 44 Years": 1804762,
+          "45 to 64 Years": 1523681,
+          "65 Years and Over": 862573
+        },
+        {
+          State: "AR",
+          "Under 5 Years": 202070,
+          "5 to 13 Years": 343207,
+          "14 to 17 Years": 157204,
+          "18 to 24 Years": 264160,
+          "25 to 44 Years": 754420,
+          "45 to 64 Years": 727124,
+          "65 Years and Over": 407205
+        },
+        {
+          State: "CA",
+          "Under 5 Years": 2704659,
+          "5 to 13 Years": 4499890,
+          "14 to 17 Years": 2159981,
+          "18 to 24 Years": 3853788,
+          "25 to 44 Years": 10604510,
+          "45 to 64 Years": 8819342,
+          "65 Years and Over": 4114496
+        },
+        {
+          State: "CO",
+          "Under 5 Years": 358280,
+          "5 to 13 Years": 587154,
+          "14 to 17 Years": 261701,
+          "18 to 24 Years": 466194,
+          "25 to 44 Years": 1464939,
+          "45 to 64 Years": 1290094,
+          "65 Years and Over": 511094
+        },
+        {
+          State: "CT",
+          "Under 5 Years": 211637,
+          "5 to 13 Years": 403658,
+          "14 to 17 Years": 196918,
+          "18 to 24 Years": 325110,
+          "25 to 44 Years": 916955,
+          "45 to 64 Years": 968967,
+          "65 Years and Over": 478007
+        }
+    ]
+  )
+
   useEffect(() => {
     const fetchData = async () => {
       setIsError(false)
@@ -224,7 +298,7 @@ const BizCharts: React.FC<{}> = (props) => {
           obj.type = 'GAAP收入'
       }
         obj.level = obj.key.split('_')[1].toUpperCase() + '级'
-        console.log(obj)
+        // console.log(obj)
         return obj;
       },
     });
@@ -270,6 +344,76 @@ const BizCharts: React.FC<{}> = (props) => {
   }
 
 
+  const updateChartR = (num: number): void => {
+    if(num == 1) {
+      setChildSource([
+        {
+            State: "11",
+            "Under 5 Years": 310504,
+            "5 to 13 Years": 552339,
+            "14 to 17 Years": 259034,
+            "18 to 24 Years": 450818,
+            "25 to 44 Years": 1231572,
+            "45 to 64 Years": 1215966,
+            "65 Years and Over": 641667
+          },
+          {
+            State: "CA",
+            "Under 5 Years": 2704659,
+            "5 to 13 Years": 4499890,
+            "14 to 17 Years": 2159981,
+            "18 to 24 Years": 3853788,
+            "25 to 44 Years": 10604510,
+            "45 to 64 Years": 8819342,
+            "65 Years and Over": 4114496
+          },
+          {
+            State: "CO",
+            "Under 5 Years": 358280,
+            "5 to 13 Years": 587154,
+            "14 to 17 Years": 261701,
+            "18 to 24 Years": 466194,
+            "25 to 44 Years": 1464939,
+            "45 to 64 Years": 1290094,
+            "65 Years and Over": 511094
+          }
+      ])
+    }
+    if(num == 2){
+      setChildSource([{
+        State: "22",
+        "Under 5 Years": 211637,
+        "5 to 13 Years": 403658,
+        "14 to 17 Years": 196918,
+        "18 to 24 Years": 325110,
+        "25 to 44 Years": 916955,
+        "45 to 64 Years": 968967,
+        "65 Years and Over": 478007
+      },
+      {
+        State: "CA",
+        "Under 5 Years": 2704659,
+        "5 to 13 Years": 4499890,
+        "14 to 17 Years": 2159981,
+        "18 to 24 Years": 3853788,
+        "25 to 44 Years": 10604510,
+        "45 to 64 Years": 8819342,
+        "65 Years and Over": 4114496
+      },
+      {
+        State: "CO",
+        "Under 5 Years": 358280,
+        "5 to 13 Years": 587154,
+        "14 to 17 Years": 261701,
+        "18 to 24 Years": 466194,
+        "25 to 44 Years": 1464939,
+        "45 to 64 Years": 1290094,
+        "65 Years and Over": 511094
+      }])
+    }
+  }
+
+
 
 
 
@@ -280,7 +424,7 @@ const BizCharts: React.FC<{}> = (props) => {
       <button onClick={() => {updateChart(1)}}>type1</button>
       <button onClick={() => {updateChart(2)}}>type2</button>
 
-
+ 
 
      {list && list.length > 1 ? (
       <ul>
@@ -329,7 +473,14 @@ const BizCharts: React.FC<{}> = (props) => {
           </Geom>
         </Chart>
 
-        <BizChartsRows  />
+
+        <br />
+
+        <button onClick={() => {updateChartR(1)}}>dv.rows11</button>
+      <button onClick={() => {updateChartR(2)}}>dv.rows22</button>
+
+
+        <BizChartsRows a={[1]} b={[2]} dataSource={childSource} />
 
     </div>
   )
